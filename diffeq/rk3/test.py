51d98bd7bcc95065464cf708d1f0810d7c340a1d
Anton Kozyriev <sonara4mus@gmail.com>
2020-10-02 20:57:23 +0300

    feat: replace unittest with pytest

--- diffeq/rk3/test.py
+++ diffeq/rk3/test.py
@@ -1,33 +1,35 @@
-from math import pow
+from math import pow, fabs
 from numpy import array, exp
 
 from diffeq.rk3.algo import rk3
 
-import unittest
+import pytest
 
 
 def get_test_error(step: float) -> float:
-    return pow(step, 1.0 / 4.0)
+    return pow(step, 1.0 / 3.0)
 
 
-def dxdy(x: float, y: array) -> array:
-    return array([y[1], -2.0 * y[1] - 2.0 * y[0] + x * exp(x)])
+def dx_dy_second_order(x: float, y: array) -> array:
+    """
+    y'' + 2y' + 2y = x * exp(x)
+    :param x:
+    :param y:
+    :return:
+    """
+    return array([y[1], -2.0 * y[1] - 2.0 * y[0] + x * exp(x)])  # z = y'
 
 
-class RungeKutta3(unittest.TestCase):
-    def positive_case_second_order_equation(self):
-        # arrange
-        test_step = 0.1
-        test_x0 = 0.0
-        test_x1 = 1.5
-        test_y0 = array([0.0, 0.0])
-        expected_y = 0.65667
+@pytest.mark.parametrize("step", [0.1, 0.01, 0.0001])
+def test_should_solve_second_order_equation_for_different_step_size(step):
+    # arrange
+    test_x0 = 0.0
+    test_x1 = 1.5
+    test_y0 = array([0.0, 0.0])  # y(0) = 0, y'(0) = 0
+    expected_y = 0.65667
 
-        # act
-        actual_y = rk3(dxdy, test_step, test_x0, test_x1, test_y0)
+    # act
+    actual_y = rk3(dx_dy_second_order, step, test_x0, test_x1, test_y0)
 
-        self.assertAlmostEquals(expected_y, actual_y, delta=get_test_error(test_step))
-
-
-if __name__ == '__main__':
-    unittest.main()
+    # assert
+    assert fabs(expected_y - actual_y) < get_test_error(step)
